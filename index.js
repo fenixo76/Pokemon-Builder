1d3b1da27ce7717d77ddc6d57db03f0a12e20791
fenixo76 <fenixo@outlook.fr>
2023-06-07 14:08:50 +0200

    Mise a jour du code

--- index.js
+++ index.js
@@ -1,35 +1,42 @@
-//Incrustation Page Pokemon 1 
-document.addEventListener('DOMContentLoaded', () => {    
-fetch("./components/pages/Pokemon_1.html")
-        .then(response => response.text())
-        .then(html => {
-            document.querySelector(".Pokemon_1").innerHTML = html;
-        })
-        .catch(error => {
-            console.log('Une erreur s\'est produite : ', error);
-        });
+window.addEventListener("load", function() {
+  var script = document.createElement("script");
+  script.src = "./components/js/Pokemon_1.js";
+  document.head.appendChild(script);
+});
 
+document.addEventListener('DOMContentLoaded', () => {
+    fetch("./components/pages/Pokemon_1.html")
+      .then(response => response.text())
+      .then(html => {
+        document.querySelector(".Pokemon_1").innerHTML = html;
+        
+      })
+      .catch(error => {
+        console.log('Une erreur s\'est produite : ', error);
+      });
+  
     fetch("./components/pages/Stats.html")
-        .then(response => response.text())
-        .then(html => {
-            document.querySelector(".Stats").innerHTML = html;
-
-            const Stats_Button = document.querySelector(".Stats_Button01");
-            const Tableau_Stats = document.getElementById("j002");
-
-            Stats_Button.addEventListener("click", function(){
-                if(Tableau_Stats.style.display === "block"){
-                    Tableau_Stats.style.display = "none";
-                    Stats_Button.innerHTML = "Afficher Les Stats"
-                } else {
-                    Tableau_Stats.style.display = "block";
-                    Stats_Button.innerHTML = "Masquer Les Stats"
-                }
-            });
-        })
-        .catch(error => {
-            console.log('Une erreur s\'est produite : ', error);
+      .then(response => response.text())
+      .then(html => {
+        document.querySelector(".Stats").innerHTML = html;
+        
+        // Ajoutez ici votre code pour gérer les événements dans Stats.html
+        const Stats_Button = document.querySelector(".Stats_Button01");
+        const Tableau_Stats = document.getElementById("j002");
+  
+        Stats_Button.addEventListener("click", function(){
+            if(Tableau_Stats.style.display === "block"){
+                Tableau_Stats.style.display = "none";
+                Stats_Button.innerHTML = "Afficher Les Stats";
+            } else {
+                Tableau_Stats.style.display = "block";
+                Stats_Button.innerHTML = "Masquer Les Stats";
+            }
         });
+      })
+      .catch(error => {
+        console.log('Une erreur s\'est produite : ', error);
+      });
 
         const test = document.querySelector(".test");
         const test2 = document.querySelector(".test2");
@@ -55,3 +62,5 @@ fetch("./components/pages/Pokemon_1.html")
 
         
     });
+
+    
