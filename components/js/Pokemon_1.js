7d2c128da6e035f4054f2613626938f9a93be62b
fenixo76 <fenixo@outlook.fr>
2023-06-09 12:40:14 +0200

    Maj V0.1

--- components/js/Pokemon_1.js
+++ components/js/Pokemon_1.js
@@ -18,24 +18,29 @@ fetch('./components/json/Pokemon_info.json')
 
             drop01.innerHTML = '';
             filteredNames.forEach(item => {
-                const listEl = document.createElement('li');
+                const pairEl = document.createElement('div');
+                pairEl.classList.add('pair'); // Ajout de la classe pair pour aligner horizontalement les éléments
+
+                const listEl = document.createElement('div');
                 listEl.classList.add('LI01');
 
-                const image = document.createElement('img');
+                const image = document.createElement('div');
                 image.classList.add('IMP01');
+
                 const index = names.findIndex(name => name === item);
                 if (index !== -1) {
                     const id = data[index].id;
-                    image.src = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`;
+                    image.style.backgroundImage = `url(https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png)`;
                 }
-                image.alt = 'Image Description';
-                drop01.appendChild(image);
+                pairEl.appendChild(image);
 
                 listEl.textContent = item;
                 if (item === userInput) {
                     listEl.classList.add('match01');
                 }
-                drop01.appendChild(listEl);
+                pairEl.appendChild(listEl);
+
+                drop01.appendChild(pairEl);
             });
 
             if (drop01.children[0] === undefined) {
@@ -46,21 +51,22 @@ fetch('./components/json/Pokemon_info.json')
             drop01.style.height = totalChildrenHeight + 'px';
         };
 
-        function bb() {
+        function disappear() {
             drop01.style.height = 0;
         }
 
         drop01.addEventListener('click', function (event) {
-            if (event.target && event.target.matches('li')) {
+            if (event.target && event.target.matches('div.LI01')) {
                 event.target.className = 'foo';
                 Recherche01.value = `${event.target.innerText}`;
-                bb();
-
+                disappear();
             }
         });
 
         Recherche01.addEventListener('input', getPokemonName);
     });
+
+
 
 var apiUrl = "https://api-pokemon-fr.vercel.app/api/v1/pokemon/";
 const IMG001 = document.querySelector("#IPKM01");
@@ -86,6 +92,87 @@ fetch('./components/json/Pokemon_info.json')
         })
     });
 
+    document.querySelector('.Image_Objet_01').addEventListener('click', function() {
+        // Basculer la classe 'active' pour afficher ou masquer l'info-bulle
+        document.querySelector('.infoBulle01').classList.toggle('active');
+      });
+
+
+
+// recupertion et Affichage des Objets
+const drop02 = document.querySelector('.drop01_02');
+const Recherche02 = document.querySelector('#Recherche02');
+
+fetch('./components/json/iteme.json')
+    .then(response => response.json())
+    .then(data => {
+        const names = data.map(item => item.Name);
+
+
+        const getObjets = async function (e) {
+            const userInput = e.target.value.toLowerCase();
+
+            if (userInput.length === 0) {
+                drop02.style.height = 0;
+                return drop02.innerHTML = '';
+            }
+
+            const filteredNames = names.filter(name => name.toLowerCase().includes(userInput)).sort().slice(0, 5);
+
+            drop02.innerHTML = '';
+            filteredNames.forEach(item => {
+                const listEl = document.createElement('li');
+                listEl.classList.add('LI02');
+                listEl.textContent = item;
+                if (item === userInput) {
+                    listEl.classList.add('match07');
+                }
+                drop02.appendChild(listEl);
+            });
+
+            if (drop02.children[0] === undefined) {
+                return drop02.style.height = 0;
+            }
+
+            let totalChildrenHeight = drop02.children[0].offsetHeight * filteredNames.length ;
+            drop02.style.height = totalChildrenHeight + 'px';
+        }
+
+        function disappear() {
+            drop02.style.height = 0;
+            return drop02.innerHTML = '';
+        }
+
+        var but02 = document.querySelector(".drop01_02");
+        but02.addEventListener("click", disappear);
+
+
+        drop02.addEventListener("click", function (event) {
+            if (event.target && event.target.matches("li")) {
+                event.target.className = "foo";
+                Recherche02.value = `${event.target.innerText}`;
+                disappear();
+            }
+        });
+
+        Recherche02.addEventListener('input', getObjets);
+    })
+
+var Objet01 = document.querySelector(".Image_Objet_01")
+var Objet001 = document.querySelector(".infoBulle01")
+
+fetch('./components/json/iteme.json')
+    .then(response => response.json())
+    .then(data => {
+
+        drop02.addEventListener("click", function (event) {
+            if (event.target) {
+                const obj = data.find(item => item.Name === Recherche02.value)
+                Objet01.src = obj.Img
+                Objet001.innerHTML = obj.Des
+            }
+        })
+    })
 
 
 
